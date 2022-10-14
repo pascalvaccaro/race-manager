2880493512d5a5b8d39404663a479876cc353d66
Pascal Vaccaro <pascal.vaccaro@gmail.com>
2022-10-14 16:26:27 +0200

    fix: development database config

--- race-admin/config/database.ts
+++ race-admin/config/database.ts
@@ -1,16 +1,10 @@
 export default ({ env }) => ({
   connection: {
-    client: 'postgres',
+    client: 'sqlite',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'pascal'),
-      password: env('DATABASE_PASSWORD', ''),
-      ssl: {
-        rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false),
-      }
+      filename: env('DATABASE_FILENAME', '.tmp/data.db'),
     },
+    useNullAsDefault: true,
     debug: false,
   },
 });
