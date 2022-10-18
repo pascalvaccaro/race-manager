822a7579f0be6580633d4ce30387a9cc95c7c403
Pascal Vaccaro <pascal.vaccaro@gmail.com>
2022-10-18 14:28:50 +0200

    revert: config sync apiToken

--- race-admin/config/plugins.ts
+++ race-admin/config/plugins.ts
@@ -7,9 +7,6 @@ export default ({ env }) => ({
     enabled: true,
     config: {
       importOnBootstrap: ["test", "staging"].includes(env("NODE_ENV")),
-      customTypes: [
-        { configName: "api-token", queryString: "admin::api-token", uid: "name" },
-      ],
       excludedConfig: [
         "core-store.plugin_strapi-stripe_stripeSetting",
         "core-store.plugin_users-permissions_grant",
